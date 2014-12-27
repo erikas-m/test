f14261955faf6aecdef248edd2ffd5a2e1f1a332
faik <faik900@gmail.com>
2014-12-27 18:38:39 +0200

    test

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 test
 ===========
 
-[![Build Status](https://travis-ci.org/erikas-m/test.svg)](https://travis-ci.org/erikas-m/test) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/erikas-m/ml_test_Erikas/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/erikas-m/ml_test_Erikas/?branch=master)
+[![Build Status](https://travis-ci.org/erikas-m/test.svg)](https://travis-ci.org/erikas-m/test) [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/erikas-m/test/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/erikas-m/test/?branch=master)
--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
-//    public function testIndex()
-//    {
-//        $client = static::createClient();
-//
-//        $crawler = $client->request('GET', '/');
-//
-//        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
-//    }
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('#form-search')->count() > 0);
+    }
 }
